873964631f24f897e4aa860df1a0b9007a4c01f1
Daniel Zimmerman <djzman078@gmail.com>
2024-12-08 15:38:04 -0600

    initial commit of template project

---
+++ main/main.c
@@ -0,0 +1,19 @@
+/*
+ * monitoring_zimknives_esp32
+ *
+ * General: this is a port of the monolithic monitoring_zimknives project to
+ * a multi-core, multi-thread, esp-idf project.
+ * 
+ * main():
+ * - this filel will serve as the "setup" file/function like arduino setup()
+ * - in addition, main() will start all of the separate threads/processes
+ * 
+ * DJZ - December 8, 2024 (c)
+ *  
+ */
+#include <stdio.h>
+
+void app_main(void)
+{
+
+}
